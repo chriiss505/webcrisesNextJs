af86782d96d678c3130545883297272e012ac7ec
cristina <cristina.sanchez@vixtechnology.com>
2025-08-06 00:06:47 +0200

    carpeta out

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // Solo activa para producción (GitHub Pages)
+  distDir: "out",
+  trailingSlash: true, // Añade esto para mejor compatibilidad
   basePath: process.env.NODE_ENV === "production" ? "/webcrisesNextJs" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/webcrisesNextJs/" : "",
   images: {
     unoptimized: true,
   },
+  // Elimina assetPrefix (puede causar problemas con export estático)
 };
+
+module.exports = nextConfig;
